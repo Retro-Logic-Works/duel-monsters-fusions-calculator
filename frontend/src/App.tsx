1450e8f1f33a5e643f9919151ea05f0454678b3e
Davin <davinjjimenez@gmail.com>
2026-08-13 21:35:52 -0700

    #20 Add LootPage.tsx, and setup the routing for it in App.tsx.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import AppHeader from './components/app-header/AppHeader'
 import SearchPage from './components/search-page/SearchPage'
+import LootPage from './components/loot-page/LootPage'
 
 function App() {
   return (
-   <SearchPage></SearchPage>
+    <BrowserRouter>
+      <AppHeader />
+      <Routes>
+        <Route path="/" element={<SearchPage />} />
+        <Route path="/loot" element={<LootPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/components/loot-page/LootPage.tsx
@@ -0,0 +1,8 @@
+import './LootPage.css'
+
+export default function LootPage() {
+    return (
+        <div className="loot-page" data-testid="loot-page">
+        </div>
+    );
+}
